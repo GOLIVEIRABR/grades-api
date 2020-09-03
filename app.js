472c3bbcbf807728f9af034f7b38b88f529e8009
Geazi Oliveira <geazioliveira@hotmail.com>
2020-09-03 03:07:43 -0300

    corrigindo id_grade

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ const app = express();
 //define o dominio de origem para consumo do servico
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: 'http://localhost:9990',
-//   })
-// );
+app.use(
+  cors({
+    origin: 'https://my-grades-app.herokuapp.com/',
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('API em execucao');
